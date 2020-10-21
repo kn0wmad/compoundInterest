baf9dc6cc36ddbe73655b2832b7c12666f6165b8
kn0wmad <kn0wmad@protonmail.com>
2020-10-21 12:41:51 -0600

    Fixed a test

--- compoundInterestFunc.py
+++ compoundInterestFunc.py
@@ -4,5 +4,5 @@ def future_value(present_value, annual_rate, periods_per_year, years):
     interest = present_value * (1 + rate_per_period)**periods
     return interest
     
-print ("$500 at 4% compounded daily for 10 years yields $", future_value(500, .04, 10, 10))
+print ("$500 at 4% compounded monthly for 10 years yields $", future_value(500, .04, 12, 10))
 print ("$1000 at 2% compounded daily for 3 years yields $", future_value(1000, .02, 365, 3))
