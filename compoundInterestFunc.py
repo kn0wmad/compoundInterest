401fd731e927ad0efb91a21d978476626edefa8e
kn0wmad <kn0wmad@protonmail.com>
2020-10-21 12:38:05 -0600

    Initial commit

---
+++ compoundInterestFunc.py
@@ -0,0 +1,8 @@
+def future_value(present_value, annual_rate, periods_per_year, years):
+    rate_per_period = annual_rate / periods_per_year
+    periods = periods_per_year * years
+    interest = present_value * (1 + rate_per_period)**periods
+    return interest
+    
+print ("$500 at 4% compounded daily for 10 years yields $", future_value(500, .04, 10, 10))
+print ("$1000 at 2% compounded daily for 3 years yields $", future_value(1000, .02, 365, 3))
